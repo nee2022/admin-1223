d43aa27cc62a7a912a5c6f570f84bdbd660563ce
zhangyihua <zhangyihua_2020@qq.com>
2020-12-24 16:54:54 +0800

    公司信息增删改查完成

--- src/pages/merchantAsset/companyInformationComponent/companyInformation.vue
+++ src/pages/merchantAsset/companyInformationComponent/companyInformation.vue
@@ -11,14 +11,20 @@
           <el-input
             placeholder="请输入账号、手机号、昵称进行查找"
             class="inputBlank"
+            v-model="input"
             clearable
           ></el-input>
         </div>
         <div class="searchButton">
-          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+          <el-button type="primary" icon="el-icon-search" @click="searchHandler"
+            >搜索</el-button
+          >
         </div>
         <div class="addButton">
-          <el-button type="primary" icon="el-icon-circle-plus-outline"
+          <el-button
+            type="primary"
+            icon="el-icon-circle-plus-outline"
+            @click="addDialogVisible = true"
             >添加</el-button
           >
         </div>
@@ -53,21 +59,31 @@
             label="操作"
             width="200"
           >
-            <div class="operation">
-              <div>
-                <img src="../../../assets/images/delete.png" title="删除" />
+            <template slot-scope="scope">
+              <div class="operation">
+                <div>
+                  <img
+                    src="../../../assets/images/delete.png"
+                    title="删除"
+                    @click="companyDeleted(scope.row.id)"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="../../../assets/images/compile.png"
+                    title="修改"
+                    @click="modifyDialogPop(scope.row)"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="../../../assets/images/see.png"
+                    title="详情"
+                    height="11px"
+                  />
+                </div>
               </div>
-              <div>
-                <img src="../../../assets/images/compile.png" title="修改" />
-              </div>
-              <div>
-                <img
-                  src="../../../assets/images/see.png"
-                  title="详情"
-                  height="11px"
-                />
-              </div>
-            </div>
+            </template>
           </el-table-column>
         </el-table>
       </template>
@@ -86,6 +102,63 @@
       >
       </el-pagination>
     </footer>
+    <template>
+      <!-- 添加网关 -->
+      <el-dialog
+        title="添加支付网关"
+        :visible.sync="addDialogVisible"
+        width="30%"
+        @close="addDialogClosed"
+      >
+        <el-form :model="addForm" ref="addFormRef" label-width="80px">
+          <el-form-item label="企业名称" prop="name">
+            <el-input v-model="addForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号" prop="telephone">
+            <el-input v-model="addForm.telephone"></el-input>
+          </el-form-item>
+          <el-form-item label="联系人" prop="contact">
+            <el-input v-model="addForm.contact"></el-input>
+          </el-form-item>
+          <el-form-item label="所属城市" prop="city">
+            <el-input v-model="addForm.city"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="addDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="companyAdded">
+            确 定
+          </el-button>
+        </span>
+      </el-dialog>
+      <!-- 修改网关 -->
+      <el-dialog
+        title="修改公司信息"
+        :visible.sync="modifyDialogVisible"
+        width="30%"
+      >
+        <el-form :model="modifyForm" ref="modifyFormRef" label-width="80px">
+          <el-form-item label="企业名称" prop="name">
+            <el-input v-model="modifyForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号" prop="telephone">
+            <el-input v-model="modifyForm.telephone"></el-input>
+          </el-form-item>
+          <el-form-item label="联系人" prop="contact">
+            <el-input v-model="modifyForm.contact"></el-input>
+          </el-form-item>
+          <el-form-item label="所属城市" prop="city">
+            <el-input v-model="modifyForm.city"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="modifyDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="companyModified">
+            确 定
+          </el-button>
+        </span>
+      </el-dialog>
+    </template>
   </div>
 </template>
 
@@ -105,12 +178,28 @@ export default {
       selected: "所有",
       pagenum: 1,
       token: "",
-      pagesize: 12
+      pagesize: 12,
+      addDialogVisible: false,
+      addForm: {
+        token: localStorage.getItem("token").replace(/\"/g, ""),
+        name: "",
+        contact: "",
+        city: ""
+      },
+      modifyDialogVisible: false,
+      modifyId: "",
+      modifyForm: {
+        token: localStorage.getItem("token").replace(/\"/g, ""),
+        name: "",
+        contact: "",
+        city: ""
+      },
+      input: ""
     };
   },
 
   mounted() {
-    this.token = localStorage.getItem("token");
+    this.token = localStorage.getItem("token").replace(/\"/g, "");
     this.getCompanyInformationMes();
   },
   methods: {
@@ -119,7 +208,7 @@ export default {
       this.$axios
         .get(
           "http://www.api.sqjtjt.com/admin/api/companies/?token=" +
-            JSON.parse(this.token) +
+            this.token +
             "&page=" +
             this.pagenum +
             "&row=12"
@@ -146,6 +235,114 @@ export default {
         ret = "否";
       }
       return ret;
+    },
+    //添加对话框关闭事件
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields();
+    },
+    //增加公司
+    companyAdded() {
+      this.$axios
+        .post("/admin/api/company", this.addForm)
+        .then(res => {
+          if (res.status !== 200) {
+            return this.$message.error("添加公司失败!");
+          }
+        })
+        .then(() => {
+          setTimeout(() => {
+            this.$router.replace("/refresh");
+          }, 500);
+        })
+        .then(() => {
+          this.$message.success("添加公司成功!");
+          this.addDialogVisible = false;
+        });
+    },
+    //删除公司
+    companyDeleted(id) {
+      let enterState = true;
+      let url = "http://www.api.sqjtjt.com/admin/api/company/" + id;
+      this.$confirm("此操作将永久删除该公司信息, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$axios.delete(url, {
+            params: {
+              token: this.token
+            }
+          });
+        })
+        .then(() => {
+          setTimeout(() => {
+            this.$router.replace("/refresh");
+          }, 500);
+        })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          if (enterState || false) {
+            this.$message({
+              type: "info",
+              message: "已取消删除"
+            });
+          } else {
+            this.$message({
+              type: "info",
+              message: "删除失败"
+            });
+          }
+        });
+    },
+    //修改公司信息
+    modifyDialogPop(row) {
+      this.modifyId = row.id;
+      this.modifyDialogVisible = true;
+    },
+    companyModified() {
+      let url = "http://www.api.sqjtjt.com/admin/api/company/" + this.modifyId;
+      this.$axios
+        .put(url, this.modifyForm)
+        .then(res => {
+          if (res.status !== 200) {
+            return this.$message.error("修改公司信息失败!");
+          }
+        })
+        .then(() => {
+          setTimeout(() => {
+            this.$router.replace("/refresh");
+          }, 500);
+        })
+        .then(() => {
+          this.$message.success("修改公司信息成功!");
+          this.addDialogVisible = false;
+        });
+    },
+
+    //查找公司
+    searchHandler() {
+      this.$axios
+        .get(
+          "http://www.api.sqjtjt.com/admin/api/companies/?token=" +
+            this.token +
+            "&page=" +
+            this.pagenum +
+            "&row=12&keyword=" +
+            this.input
+        )
+        .then(res => {
+          if (res.status == 200) {
+            this.tableData = res.data.companies;
+            this.total = res.data.total || 0;
+            var pn = this.pagenum;
+          }
+        });
     }
   }
 };
