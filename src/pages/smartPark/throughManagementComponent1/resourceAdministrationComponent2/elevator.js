d429a0981faa6959efeb6ab4ac06f728fac49431
zhangyihua <zhangyihua_2020@qq.com>
2021-01-13 13:52:39 +0800

    elevator

--- src/pages/smartPark/throughManagementComponent1/resourceAdministrationComponent2/elevator.js
+++ src/pages/smartPark/throughManagementComponent1/resourceAdministrationComponent2/elevator.js
@@ -14,8 +14,8 @@ export default {
       selected: "所有",
       pagenum: 1,
       token: "",
-      pagesize: 12,
-      chart: false
+      pagesize: 18,
+      chart: true
     };
   },
 
@@ -25,26 +25,50 @@ export default {
   },
   methods: {
     toggleListTable() {
+      this.pagesize = 12;
+      this.getAllInvoiceRecordMes();
       this.chart = false;
     },
     toggleChartTable() {
+      this.pagesize = 18;
+      this.getAllInvoiceRecordMes();
       this.chart = true;
     },
     //获取用户信息列表
-    getAllInvoiceRecordMes() {
+    getAllInvoiceRecordMes(resolve, reject) {
       this.$axios
         .get(
           "/admin/api/chargers/1/?token=" +
             this.token +
             "&page=" +
             this.pagenum +
-            "&row=12"
+            "&row=" +
+            this.pagesize
         )
         .then(res => {
           if (res.status == 200) {
             this.tableData = res.data.chargers;
+            for (let i = 0; i < this.tableData.length; i++) {
+              if (i === 0) {
+                this.tableData[i].styleId = i + 1;
+              } else {
+                this.tableData[i].styleId = i;
+              }
+              if (i < 4) {
+                this.tableData[i].floorArray = [1, 2, 3, 4, 5, 6, 7];
+              } else {
+                this.tableData[i].floorArray = [
+                  i - 3,
+                  i - 2,
+                  i - 1,
+                  i,
+                  i + 1,
+                  i + 2,
+                  i + 3
+                ];
+              }
+            }
             this.total = res.data.total || 0;
-            var pn = this.pagenum;
           }
         });
     },
--- src/pages/smartPark/throughManagementComponent1/resourceAdministrationComponent2/elevator.vue
+++ src/pages/smartPark/throughManagementComponent1/resourceAdministrationComponent2/elevator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="zyhSingleLineListMainPage">
+  <div class="zyhSingleLineListMainPage elevator">
     <header>
       <div class="infoArea">
         <div class="pageName">电梯</div>
@@ -19,7 +19,6 @@
         <div class="searchButton">
           <el-button type="primary" icon="el-icon-search">搜索</el-button>
         </div>
-        <div class="blank"></div>
         <div class="toggleTable">
           <div v-if="chart" class="listButton" @click="toggleListTable">
             <img src="../../../../assets/images/listIcon.png" alt="" />
@@ -48,8 +47,8 @@
           </el-table-column>
           <el-table-column show-overflow-tooltip prop="agent" label="所属商户">
           </el-table-column>
-          <el-table-column show-overflow-tooltip prop="address" label="地址"
-            >浙江省杭州市金沙大道888号
+          <el-table-column show-overflow-tooltip prop="address" label="地址">
+            浙江省杭州市金沙大道888号
           </el-table-column>
           <el-table-column
             show-overflow-tooltip
@@ -69,7 +68,109 @@
           </el-table-column>
         </el-table>
       </template>
-      <template v-if="chart"><div>1</div></template>
+      <template v-if="chart">
+        <ul class="chartItems">
+          <li v-for="item in tableData" :key="item.id">
+            <div class="tableItem1" v-if="item.styleId < 16">
+              <div class="signBar">
+                <span class="circle"></span>
+                <span>{{ item.id }}</span>
+                <span>
+                  <img src="../../../../assets/images/signal4.png" alt="" />
+                </span>
+              </div>
+              <div class="info">
+                <span class="info1">东3梯</span>
+                <span class="info2">{{ item.styleId }}/23</span>
+                <div class="floor">
+                  <span
+                    v-for="childItem in item.floorArray"
+                    :key="item.styleId"
+                  >
+                    {{ childItem }}
+                  </span>
+                </div>
+              </div>
+              <div class="operation">
+                <div>
+                  <img
+                    src="../../../../assets/images/Tdelete.png"
+                    title="删除"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="../../../../assets/images/Teditor.png"
+                    title="修改"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="tableItem2" v-if="item.styleId === 16">
+              <div class="signBar">
+                <span class="circle"></span>
+                <span>{{ item.id }}</span>
+                <span>
+                  <img src="../../../../assets/images/signal4.png" alt="" />
+                </span>
+              </div>
+              <div class="info">
+                <span class="info1">东3梯</span>
+                <span class="info2">{{ item.styleId }}/23</span>
+                <div class="floor">
+                  <span
+                    v-for="childItem in item.floorArray"
+                    :key="item.styleId"
+                  >
+                    {{ childItem }}
+                  </span>
+                </div>
+              </div>
+              <div class="operation">
+                <div>
+                  <img
+                    src="../../../../assets/images/Tdelete.png"
+                    title="删除"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="../../../../assets/images/Teditor.png"
+                    title="修改"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="tableItem3" v-if="item.styleId === 17">
+              <div class="signBar">
+                <span class="circle"></span>
+                <span>{{ item.id }}</span>
+                <span>
+                  <img src="../../../../assets/images/signal0.png" alt="" />
+                </span>
+              </div>
+              <div class="info">
+                <span>设备已离线</span>
+              </div>
+              <div class="line"></div>
+              <div class="operation">
+                <div>
+                  <img
+                    src="../../../../assets/images/Tdelete.png"
+                    title="删除"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="../../../../assets/images/Teditor.png"
+                    title="修改"
+                  />
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </template>
     </section>
     <footer>
       <div class="total" :data="tableData">
@@ -90,6 +191,7 @@
 
 <script>
 import elevator from "./elevator.js";
+
 export default elevator;
 </script>
 
