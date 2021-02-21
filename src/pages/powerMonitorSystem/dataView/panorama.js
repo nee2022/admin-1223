cba0a29c6a032f4b962b42951baeb4e73a8182ee
zhangyihua <zhangyihua_2020@qq.com>
2021-02-21 13:21:00 +0800

    海量标记

--- src/pages/powerMonitorSystem/dataView/panorama.js
+++ src/pages/powerMonitorSystem/dataView/panorama.js
@@ -7,58 +7,50 @@ export default {
   },
   data() {
     return {
-      data: [],
-      map: {}
+      chargers: []
     };
   },
   mounted() {
-    this.getMassMarksData();
-    this.generateMap();
-    this.addMassMarks();
-  },
-  methods: {
-    getMassMarksData() {
-      this.$axios.get(`/map/gd/chargers/5`).then(res => {
-        this.data = res.data.chargers;
-      });
-    },
-    generateMap() {
-      this.map = new AMap.Map("container", {
-        resizeEnable: true,
-        center: [116.397428, 39.90923], //地图中心点
-        zoom: 13 //地图显示的缩放级别
-      });
-    },
-    addMassMarks() {
-      // 创建样式对象
-      var styleObject = {
-        url: require("../../../assets/images/DCicon.png"),
-        size: new AMap.Size(11, 11), // 图标大小
-        anchor: new AMap.Pixel(5, 5) // 图标显示位置偏移量，基准点为图标左上角
-      };
+    this.$axios.get(`/map/gd/chargers/8,9`).then(res => {
+      this.chargers = res.data.chargers;
+    });
+    var map = new AMap.Map("container", {
+      zoom: 4,
+      center: [102.342785, 35.312316]
+    });
+    var style = [];
+    sytle[8] = {
+      url: require("../../../assets/images/ACS.png"),
+      anchor: new AMap.Pixel(6, 6),
+      size: new AMap.Size(11, 11)
+    };
+    sytle[9] = {
+      url: require("../../../assets/images/DCicon.png"),
+      anchor: new AMap.Pixel(6, 6),
+      size: new AMap.Size(11, 11)
+    };
+    var mass = new AMap.MassMarks(this.chargers, {
+      opacity: 0.8,
+      zIndex: 111,
+      cursor: "pointer",
+      style: style
+    });
 
-      var massMarks = new AMap.MassMarks({
-        zIndex: 5, // 海量点图层叠加的顺序
-        zooms: [3, 19], // 在指定地图缩放级别范围内展示海量点图层
-        style: styleObject // 设置样式对象
-      });
-      var data = [
-        {
-          lnglat: [116.405285, 39.904989], // 点标记位置
-          name: "beijing",
-          id: 1
-        },
-        {
-          lnglat: [226.405285, 139.904989], // 点标记位置
-          name: "beijing",
-          id: 2
-        }
-      ];
+    var marker = new AMap.Marker({ content: " ", map: map });
 
-      massMarks.setData(data);
+    mass.on("mouseover", function(e) {
+      marker.setPosition(e.data.lnglat);
+      marker.setLabel({ content: e.data.name });
+    });
 
-      // 将海量点添加至地图实例
-      massMarks.setMap(this.map);
+    mass.setMap(map);
+
+    function setStyle(multiIcon) {
+      if (multiIcon) {
+        mass.setStyle(style);
+      } else {
+        mass.setStyle(style[2]);
+      }
     }
   }
 };
