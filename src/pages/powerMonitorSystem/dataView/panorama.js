c43aeafcbc92c7bd472283ee6e8f1f7d4ba2ef02
zhangyihua <zhangyihua_2020@qq.com>
2021-02-22 13:42:29 +0800

    完成电力海量标记

--- src/pages/powerMonitorSystem/dataView/panorama.js
+++ src/pages/powerMonitorSystem/dataView/panorama.js
@@ -13,44 +13,40 @@ export default {
   mounted() {
     this.$axios.get(`/map/gd/chargers/8,9`).then(res => {
       this.chargers = res.data.chargers;
-    });
-    var map = new AMap.Map("container", {
-      zoom: 4,
-      center: [102.342785, 35.312316]
-    });
-    var style = [];
-    sytle[8] = {
-      url: require("../../../assets/images/ACS.png"),
-      anchor: new AMap.Pixel(6, 6),
-      size: new AMap.Size(11, 11)
-    };
-    sytle[9] = {
-      url: require("../../../assets/images/DCicon.png"),
-      anchor: new AMap.Pixel(6, 6),
-      size: new AMap.Size(11, 11)
-    };
-    var mass = new AMap.MassMarks(this.chargers, {
-      opacity: 0.8,
-      zIndex: 111,
-      cursor: "pointer",
-      style: style
-    });
 
-    var marker = new AMap.Marker({ content: " ", map: map });
+      var map = new AMap.Map("container", {
+        zoom: 4,
+        center: [0, 0]
+      });
+      var style = [];
 
-    mass.on("mouseover", function(e) {
-      marker.setPosition(e.data.lnglat);
-      marker.setLabel({ content: e.data.name });
+      style[8] = {
+        url: require("../../../assets/images/ACS.png"),
+        anchor: new AMap.Pixel(6, 6),
+        size: new AMap.Size(50, 50)
+      };
+      style[9] = {
+        url: require("../../../assets/images/DCicon.png"),
+        anchor: new AMap.Pixel(6, 6),
+        size: new AMap.Size(50, 50)
+      };
+      // console.log(style);
+      // console.log(this.chargers);
+      var mass = new AMap.MassMarks(this.chargers, {
+        opacity: 0.8,
+        zIndex: 111,
+        cursor: "pointer",
+        style: style
+      });
+
+      var marker = new AMap.Marker({ content: " ", map: map });
+
+      mass.on("mouseover", function(e) {
+        marker.setPosition(e.data.lnglat);
+        marker.setLabel({ content: e.data.name });
+      });
+
+      mass.setMap(map);
     });
-
-    mass.setMap(map);
-
-    function setStyle(multiIcon) {
-      if (multiIcon) {
-        mass.setStyle(style);
-      } else {
-        mass.setStyle(style[2]);
-      }
-    }
   }
 };
